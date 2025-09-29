ea2d51ab44943c997eb97d6ca306a6f12e41a21f
Illuru Yadhu Vamsi <yadhuvamsiilluru@gmail.com>
2025-09-29 18:37:17 +0530

    Update resources.tf

--- Sample-WebApplicationEnd2End/Terraform/resources.tf
+++ Sample-WebApplicationEnd2End/Terraform/resources.tf
@@ -24,3 +24,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 }
+# enabling acrpull permission as the image has to be pushed to aks
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
